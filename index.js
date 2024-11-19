6bd66aa89212c0f9f368ee29ec236778d008fe38
joksdemic <jovanademic2@gmail.com>
2024-11-19 12:18:11 +0100

    Add function to generate group stage match results

--- index.js
+++ index.js
@@ -11,5 +11,20 @@ function generateMatchResults(teamA, teamB) {
         scoreTeamA,
         scoreTeamB
     };
-
 }
+
+function generateGroupStageResults(groups) {
+    const results = {};
+    for(const[group, teams] of Object.entries(groups)) {
+        results[group] = [];
+        for(let i = 0; i < teams.length; i++) {
+            for(let j = i + 1; j < teams.length; j++) {
+                const match = generateMatchResults(teams[i],teams[j]);
+                results[group].push(match);
+            }
+        }
+    }
+    return results;
+}
+
+const results = generateGroupStageResults(groups);
