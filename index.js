3b223f1d4916c40df4c76e02a3090a9171281bec
joksdemic <jovanademic2@gmail.com>
2024-11-19 12:10:04 +0100

    Add function to generate match results with random scores

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const fs = require("fs");
 const groups = JSON.parse(fs.readFileSync("groups.json","utf-8"));
+
+function generateMatchResults(teamA, teamB) {
+    const scoreTeamA = Math.floor(Math.random() * 30) + 70;
+    const scoreTeamB = Math.floor(Math.random() * 30) + 70;
+
+    return {
+        teamA: teamA.Team,
+        teamB: teamB.Team,
+        scoreTeamA,
+        scoreTeamB
+    };
+
+}
